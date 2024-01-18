b3320fe44fd52bc11f21cd1d2a51975ea03a05dd
Gageinator <124416290+Gageinator@users.noreply.github.com>
2024-01-18 16:05:57 -0500

    Renamed the files and added a class to the pthon file ProfilePage.

---
+++ src/ProfilePage.py
@@ -0,0 +1,39 @@
+import tornado.web
+import random
+
+D = {
+    "alice":
+    {
+        "name" : "Alice Smith",
+        "DOB" : "Jan.1",
+        "email" : "alice@example.com"
+    },
+    "bob":
+    {
+        "name" : "Bob Jones",
+        "DOB" : "Dec.31",
+        "email" : "bob@bob.xyz"
+    },
+    "carol":
+    {
+        "name" : "Carol Ling",
+        "DOB" : "Jul.17",
+        "email" : "carol@example.com"
+    },
+    "dave":
+    {
+        "name" : "Dave N. Port",
+        "DOB" : "Mar.14",
+        "email" : "dave@dave.dave"
+    }
+}
+
+class ProfileHandler(tornado.web.RequestHandler):
+    def get(self):
+        L = self.request.path.split("/")
+        uname = L[2]
+        info = D[uname]
+        self.render("profilepage.html",
+                    name=info["name"], dateOfBirth=info["dob"],
+                    email=info["email"])
+        
